707e98db8f0c68c2419e0ba0aa9f480e76c1f42e
michal_urbanski <michal_urbanski@windowslive.com>
2019-03-23 17:14:24 +0100

    Orders by the most oldest games played

--- bkgames/games_history/games_history_operations.py
+++ bkgames/games_history/games_history_operations.py
@@ -3,9 +3,27 @@ class GamesHistoryOperations:
     @staticmethod
     def order_by_games_played(team_frequency):
         # TODO: add docstring
+        # Team with the most games played is at the top
         results = list()
         
         for entry in team_frequency.items():
             results.append((entry[0], len(entry[1]), entry[1]))
+
+        return sorted(results, key = lambda x: x[1], reverse = True)
+
+    @staticmethod
+    def order_by_oldest_games(team_frequency):
+        """
+        Returns list of tuples ("team_name", date_of_the_most_recent_game)
+        Results are sorted by the oldest game played among all the teams
+        """
+        
+        results = list()
+
+        for entry in team_frequency.items():
+            games_dates = entry[1]
+            sorted_by_oldest = sorted(games_dates, reverse = True)
+            first_oldest_game_by_the_team = sorted_by_oldest[0]
+            results.append((entry[0], first_oldest_game_by_the_team))
 
         return sorted(results, key = lambda x: x[1], reverse = True)
--- bkgames/test/test_games_history_operations.py
+++ bkgames/test/test_games_history_operations.py
@@ -19,4 +19,18 @@ class TestGamesHistoryOperations(unittest.TestCase):
         second_team = most_played_games[-1]
         self.assertGreaterEqual(first_team[1], second_team[1])
     
+    def test_order_by_oldest_games(self):
+        oldest_played_games = GamesHistoryOperations.order_by_oldest_games(self.team_frequency)
+
+        first_team = oldest_played_games[0]
+        second_team = oldest_played_games[1]
+
+        self.assertEqual(first_team[0], "first_team")
+        self.assertEqual(second_team[0], "second_team")
+
+        
+
+
+
+
     # TODO: test case for situation when there's only one entry (KeyError handling)
