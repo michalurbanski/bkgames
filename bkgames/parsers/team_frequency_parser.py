15a075f52624cab28a179ea9a08c4b3e52c420f6
michal_urbanski <michal_urbanski@windowslive.com>
2019-03-25 20:40:56 +0100

    Parsing line algorithm finds home team, away team, and game date

--- bkgames/parsers/team_frequency_parser.py
+++ bkgames/parsers/team_frequency_parser.py
@@ -1,11 +1,27 @@
 from datetime import datetime
+import re
 
 class TeamFrequencyParser:
     
     @staticmethod
     def Parse(line):
+        """
+        Expected format is day.month (without year)
+        Example: DONE - Nba game 16.10 bos at phi -> bos?
+        """
+        date_search = re.findall(r"\d{2}", line, flags=re.I)
+        
+        #TODO: year value should be take from config, maybe callable class would be good for this?
+        date = datetime(2018, int(date_search[1]), int(date_search[0]))
+
+        # Get what's after the date
+        skip_after = date_search[0] + "." + date_search[1]
+        end_pos = re.search(skip_after, line).end()
+        split = re.split(r"\s", line[end_pos:])
+        remaining_list = list(filter(None, split)) # Clean empty strings
+        
         return {
-                "home_team": "phi", 
-                "away_team": "bos",
-                "date": datetime(2018, 10, 16)
+                "home_team": remaining_list[0], #[1] is 'at' word
+                "away_team": remaining_list[2],
+                "date": date
                 }
--- bkgames/test/test_team_frequency_parser.py
+++ bkgames/test/test_team_frequency_parser.py
@@ -12,3 +12,13 @@ class TestTeamFrequencyParser(unittest.TestCase):
         self.assertEqual(result["away_team"], "bos")
         # TODO: to be set somewhere in config for which year application should run
         self.assertEqual(result["date"], datetime(2018, 10, 16))
+
+    def test_parse_line_with_standings_returns_teams_and_date_of_the_game(self):
+        input = "DONE - Nba game 12.12 det at cha -> cha (cha 14:13; det 13:13) "
+        result = TeamFrequencyParser.Parse(input)
+
+        self.assertEqual(result["home_team"], "det")
+        self.assertEqual(result["away_team"], "cha")
+        self.assertEqual(result["date"], datetime(2018, 12, 12))
+
+    # TODO: add more examples of lines (like the one with results, and check if it works OK)
