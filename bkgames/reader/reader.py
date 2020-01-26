91a643df73dfbb7d84f886bf2c955e975d8f9b10
michal_urbanski <michal_urbanski@windowslive.com>
2020-01-26 21:09:07 +0100

    Changes variable to private

--- bkgames/reader/reader.py
+++ bkgames/reader/reader.py
@@ -1,15 +1,15 @@
 class Reader:
     
     def __init__(self, path_to_file):
-        self.path_to_file = path_to_file
+        self._path_to_file = path_to_file
 
     def read(self):
         """ 
-        First simple implementation reads all lines at once to list
+        First simple implementation reads all lines at once to list.
         """
         lines = []
 
-        with open(self.path_to_file, mode = "r") as f:
+        with open(self._path_to_file, mode = "r") as f:
             for line in f:
                 lines.append(line.strip())
 
