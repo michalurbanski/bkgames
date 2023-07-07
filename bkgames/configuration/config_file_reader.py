6642da5158afcf0bdd6079c7e20c885a075aedd5
michal_urbanski <michal_urbanski@windowslive.com>
2023-07-07 21:01:07 +0200

    Refactors class for reading config file

--- bkgames/configuration/config_file_reader.py
+++ bkgames/configuration/config_file_reader.py
@@ -3,11 +3,15 @@ import json
 
 
 class ConfigFileReader:
-    def __init__(self, filename: str):
-        self._filename = filename
+    def __init__(self, file_path: str):
+        self._file_path = file_path
+
+    def read(self, config: Config) -> None:
+        content = self._read_file()
+        ConfigFileReader._populate_config(content, config)
 
     def _read_file(self):
-        with open(self._filename, "r") as f:
+        with open(self._file_path, "r") as f:
             return json.load(f)
 
     @classmethod
@@ -16,7 +20,3 @@ class ConfigFileReader:
         config.allowed_teams = content.get("allowed_teams", [])
         config.season_start_month = content.get("season_start_month", 0)
         config.skipped_teams = content.get("skipped_teams", [])
-
-    def read(self, config: Config) -> None:
-        content = self._read_file()
-        ConfigFileReader._populate_config(content, config)
