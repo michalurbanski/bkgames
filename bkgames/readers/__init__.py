bef145d0b431183ebd018bfc5a3ba62e28183ac4
michal_urbanski <michal_urbanski@windowslive.com>
2023-11-23 22:59:18 +0100

    Removes no longer needed files

--- bkgames/readers/__init__.py
+++ bkgames/readers/__init__.py
@@ -1,5 +1,2 @@
-from .autofinder import Autofinder
-from .file_finder_factory import FileFinderFactory
-from .file_reader import FileReader
 from .team_model import TeamModel
 from .simple_file_reader import SimpleFileReader
--- bkgames/readers/file_finder.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class FileFinder(ABC):
-    """ Base class for file finders"""
-
-    @abstractmethod
-    def find_input_file(self) -> str:
-        pass
