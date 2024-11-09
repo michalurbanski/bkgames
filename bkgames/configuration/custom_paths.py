a8c04efbb72b24de9b52edd42bec59d4cc7ab0d9
michal_urbanski <michal_urbanski@windowslive.com>
2024-11-09 23:57:36 +0100

    Improvements to getting applications's paths

--- bkgames/configuration/custom_paths.py
+++ bkgames/configuration/custom_paths.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 
 class CustomPaths:
@@ -6,12 +7,16 @@ class CustomPaths:
     Paths are not configurable because there's no need to.
     They could be taken from .json file though, if ever needed.
 
-    Paths of interest, in this implementation, are stored in the application folder,
-    that's inside the user folder.
+    Data for the application is / should be stored in the .bkgames/data folder inside
+    user's folder.
+    Inside the .bkgames folder, there should be config.json file.
+
+    If there's no config.json file, then it's initialized with the default data
+    when the application runs for the first time.
     """
 
     def __init__(self):
-        self._configuration_root_path = os.path.expanduser("~")  # User's folder.
+        self._configuration_root_path = Path.home() # User's folder
         self._application_folder_path = os.path.join(
             self._configuration_root_path,
             ".bkgames",
@@ -19,22 +24,22 @@ class CustomPaths:
         self._config_file_name = "config.json"
 
     @property
-    def application_folder_path(self):
+    def application_folder_path(self) -> str:
         return self._application_folder_path
 
     @property
-    def config_file_name(self):
+    def config_file_name(self) -> str:
         return self._config_file_name
 
     @property
-    def config_path(self):
+    def config_path(self) -> str:
         return os.path.join(
             self._application_folder_path,
             self._config_file_name,
         )
 
     @property
-    def data_folder_path(self):
+    def data_folder_path(self) -> str:
         return os.path.join(
             self._application_folder_path,
             "data",
