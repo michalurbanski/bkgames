2f93d199de77dbfc4fc954034c2d696188143d95
michal_urbanski <michal_urbanski@windowslive.com>
2025-01-03 00:10:17 +0100

    Makes methods more predictable by returning the copy of the passed list

--- bkgames/dataenhancers/not_yet_played_enhancer.py
+++ bkgames/dataenhancers/not_yet_played_enhancer.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 from bkgames.configuration import Config
 from bkgames.models import TeamModel
@@ -7,22 +8,26 @@ class NotYetPlayedEnhancer:
     def enhance_data(self, input: List[TeamModel], config: Config) -> List[TeamModel]:
         """
         When team has not played yet, it's not in the results (input to this function).
-        Enhance results by adding those teams that have 0 games.
+        Enhance results by adding those teams that have 0 games played,
+        so that they show up in the results.
         """
 
         allowed_teams = config.allowed_teams
+        results = copy.deepcopy(input)
+
         if len(allowed_teams) != len(input):
             teams_that_played = [team.team_code for team in input]
             missing_teams = self._find_difference(allowed_teams, teams_that_played)
 
             for team in missing_teams:
-                input.append(TeamModel(team))
+                results.append(TeamModel(team))
 
-        return input
+        return results
 
     @staticmethod
     def _find_difference(first: List[str], second: List[str]) -> List[str]:
-        """first list has to contain the second list"""
+        """First list has to be a superset of the second list to get any meaningful results.
+        """
 
         # Note: there's even better solution to find difference:
         # https://stackoverflow.com/a/3462202
--- bkgames/dataenhancers/skip_teams_enhancer.py
+++ bkgames/dataenhancers/skip_teams_enhancer.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 from bkgames.models import TeamModel
 from bkgames.configuration import Config
@@ -7,7 +8,11 @@ class SkipTeamsEnhancer:
     """Skips some teams from watching, based on config file."""
 
     def enhance_data(self, input: List[TeamModel], config: Config) -> List[TeamModel]:
-        for team in input:
-            if team.team_code in config.skipped_teams:
-                team.skip_from_watching = True
-        return input
+        results = copy.deepcopy(input)
+
+        if (config.skipped_teams):
+            for team in results:
+                if team.team_code in config.skipped_teams:
+                    team.skip_from_watching = True
+
+        return results
