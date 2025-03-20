6ffe40b5bcd15d0d8ef98e34448b95c8bf337dfe
michal_urbanski <michal_urbanski@windowslive.com>
2025-03-20 23:49:31 +0100

    Starts refactoring of initializer

--- bkgames/configuration/initializer.py
+++ bkgames/configuration/initializer.py
@@ -5,51 +5,47 @@ from . import constants
 import importlib.resources
 
 
+# TODO: improve class description
 class Initializer:
     """Initializes application configuration, in the application folder,
     inside the users's home folder.
 
     - Copies config.json file from the package to the user's home folder.
     Users can then easily set their own configuration settings,
-    by modifying config.json content,
+    by modifying the config.json content,
     - Creates folder for data.
     """
 
     def __init__(self, custom_paths: CustomPaths):
-        # TODO: no need to store these values in separate fields. Looks strange.
-        self._application_folder_path = custom_paths.application_folder_path
-        self._config_file_name = constants.CONFIG_FILE_NAME
-        self._config_file_path = custom_paths.config_path
-        self._data_folder_path = custom_paths.data_folder_path
+        self._custom_paths = custom_paths
 
     def initialize(self) -> None:
         self._copy_config()
         self._create_data_folder()
 
-    # TODO: could be further split into smaller methods
+    # TODO: could be further split into smaller method
     def _copy_config(self) -> None:
         # Get a config.json file from the package.
-        # TODO: replace hardcoded strings, now there's a constants.py file
-        config_source_path = importlib.resources.files("bkgames") / "config.json"
+        config_source_path = importlib.resources.files(constants.MODULE_NAME) / constants.CONFIG_FILE_NAME
 
         if not config_source_path:
             raise Exception("Cannot find configuration file in the package")
 
-        if not os.path.exists(self._application_folder_path):
-            os.mkdir(self._application_folder_path)
-            print(f"Configuration folder created at {self._application_folder_path}")
+        if not os.path.exists(self._custom_paths.application_folder_path):
+            os.mkdir(self._custom_paths.application_folder_path)
+            print(f"Configuration folder created at {self._custom_paths.application_folder_path}")
 
-        if not os.path.exists(self._config_file_path):
-            shutil.copyfile(config_source_path, self._config_file_path)
-            print(f"Configuration file initialized. Location: {self._config_file_path}")
+        if not os.path.exists(self._custom_paths.config_path):
+            shutil.copyfile(config_source_path, self._custom_paths.config_path)
+            print(f"Configuration file initialized. Location: {self._custom_paths.config_path}")
         else:
             print(
-                f"Configuration file already exists. Skipping creation. Location: {self._config_file_path}"
+                f"Configuration file already exists. Skipping creation. Location: {self._custom_paths.config_path}"
             )
 
     def _create_data_folder(self) -> None:
-        if os.path.exists(self._data_folder_path):
-            print(f"Data folder exists at {self._data_folder_path}")
+        if os.path.exists(self._custom_paths.data_folder_path):
+            print(f"Data folder exists at {self._custom_paths.data_folder_path}")
         else:
-            os.mkdir(self._data_folder_path)
-            print(f"Data folder created at {self._data_folder_path}")
+            os.mkdir(self._custom_paths.data_folder_path)
+            print(f"Data folder created at {self._custom_paths.data_folder_path}")
