4e69364e711b6d1ebc5a830050e4daa7aa3e5ca6
michal_urbanski <michal_urbanski@windowslive.com>
2025-05-25 21:19:39 +0200

    Extracts method to load data

--- bkgames/app.py
+++ bkgames/app.py
@@ -1,12 +1,14 @@
 # (info) With implicit packages it would work like this.
 #        So the specific file name would have to be provided.
 # from bkgames.configuration.config_file_reader import Config
+from typing import List
 from bkgames.configuration import (
     ConfigFileReader,
     Initializer,
     ApplicationPaths,
     DataFinder,
 )
+from bkgames.configuration.config import Config
 from bkgames.readers import SimpleFileReader
 from bkgames.parsers import FileParser, TeamFrequencyParser
 from bkgames.validators import TeamsValidator
@@ -21,6 +23,16 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 
+def load_data(
+    config: Config, app_paths: ApplicationPaths, logger: logging.Logger
+) -> List[str]:
+    data_file_path = DataFinder(config, app_paths).find_data_path()
+
+    logger.info(f"Path to the file with data is: {data_file_path}")
+
+    return SimpleFileReader(data_file_path).read()
+
+
 def run():
     app_paths = ApplicationPaths()
     Initializer(app_paths).initialize()
@@ -28,11 +40,7 @@ def run():
     logger.info(f"Reading configuration file: {app_paths.config_path} ...")
 
     config = ConfigFileReader(app_paths.config_path).read()
-    data_file_path = DataFinder(config, app_paths).find_data_path()
-
-    logger.info(f"Path to the file with data is: {data_file_path}")
-
-    lines = SimpleFileReader(data_file_path).read()
+    lines = load_data(config, app_paths, logger)
 
     print("Printing the least recently played teams at the bottom...")
 
