4253b5095a28ee4ff4ad8f1b0f2e7fb26b3a49a2
michal_urbanski <michal_urbanski@windowslive.com>
2025-09-11 23:25:08 +0200

    More readable functions

--- bkgames/parsers/raw_line_parser.py
+++ bkgames/parsers/raw_line_parser.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from bkgames.parsers import LineParserBase, FileParsingResult
 from bkgames.validators import TeamsValidator
 
@@ -25,15 +25,18 @@ class RawLineParser:
         results = FileParsingResult()
 
         for line in lines:
-            # TODO: data is passed twice below? + if/else could be simplified?
-            parsing_status, parsed_data = self._lines_parser.parse(line)
+            parsing_status, parsed_data = self._parse_line(line)
             if parsing_status:
-                validation_status, data = self._teams_validator.validate(parsed_data)
-                if validation_status:
-                    results.parsed_lines.append(data)
-                else:
-                    results.not_parsed_lines.append(data)
+                results.parsed_lines.append(parsed_data)
             else:
                 results.not_parsed_lines.append(parsed_data)
 
         return results
+
+    # TODO: here this return type could be also improved
+    def _parse_line(self, line: str) -> Tuple[bool, dict]:
+        parsing_status, parsed_data = self._lines_parser.parse(line)
+        if parsing_status:
+            return self._teams_validator.validate(parsed_data)
+
+        return parsing_status, parsed_data
--- bkgames/validators/teams_validator.py
+++ bkgames/validators/teams_validator.py
@@ -1,15 +1,15 @@
-from typing import List
+from typing import List, Tuple
 
 
 class TeamsValidator:
-    """ Checks if team code is valid, based on configuration list """
+    """Checks if team code is valid, based on configuration list"""
 
     def __init__(self, valid_team_codes: List[str]):
-        """ valid_team_codes - List of valid team codes """
+        """valid_team_codes - List of valid team codes"""
         self._valid_team_codes = valid_team_codes
 
-    def validate(self, previous_parsing_result: dict) -> (bool, dict):
-        """ previous_parsing_result is a dictionary as follows
+    def validate(self, previous_parsing_result: dict) -> Tuple[bool, dict]:
+        """previous_parsing_result is a dictionary as follows
 
         {
             'home_team',
@@ -20,16 +20,21 @@ class TeamsValidator:
         }
         """
 
-        teams = [previous_parsing_result["home_team"],
-                 previous_parsing_result["away_team"]]
+        teams = [
+            previous_parsing_result["home_team"],
+            previous_parsing_result["away_team"],
+        ]
 
         invalid_teams = self._validate_teams(teams)
 
         if invalid_teams:
-            return (False, {
-                "not_parsed": previous_parsing_result["line"],
-                "error": "Incorrect team codes found: {}".format(invalid_teams)
-            })
+            return (
+                False,
+                {
+                    "not_parsed": previous_parsing_result["line"],
+                    "error": "Incorrect team codes found: {}".format(invalid_teams),
+                },
+            )
         else:
             return (True, previous_parsing_result)
 
@@ -37,8 +42,11 @@ class TeamsValidator:
         invalid_teams = []
 
         for team in teams:
-            valid_team = [True for team_code in self._valid_team_codes
-                          if TeamsValidator._is_team_codes_match(team, team_code)]
+            valid_team = [
+                True
+                for team_code in self._valid_team_codes
+                if TeamsValidator._is_team_codes_match(team, team_code)
+            ]
             if not valid_team:
                 invalid_teams.append(team)
 
