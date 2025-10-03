87a1de3fc5b7455a055356460bbe8cd32fc7079d
michal_urbanski <michal_urbanski@windowslive.com>
2025-10-03 22:51:30 +0200

    Improves config class, with an important learning

--- bkgames/configuration/config.py
+++ bkgames/configuration/config.py
@@ -1,8 +1,11 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+
+# ! default_factory is needed, because of python behavior that default arguments of a function are shared between instances.
+# ! See also: https://www.pythonmorsels.com/mutable-default-arguments/
 class Config(BaseModel):
     data_file_regexp: str
-    allowed_teams: List[str]
     season_start_month: int
-    skipped_teams: List[str]
+    allowed_teams: List[str] = Field(default_factory=list)
+    skipped_teams: List[str] = Field(default_factory=list)
